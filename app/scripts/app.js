ae2d63ed9566f4516e5cda7034112f160a9bd46c
baukebrenninkmeijer <bauke.brenninkmeijer@gmail.com>
2017-05-08 16:20:27 +0200

    Initial setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wiehaaltdepitcherApp
+ * @description
+ * # wiehaaltdepitcherApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wiehaaltdepitcherApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wiehaaltdepitcherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wiehaaltdepitcherApp
+ */
+angular.module('wiehaaltdepitcherApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
