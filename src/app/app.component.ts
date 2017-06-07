2723491cf21c2687228c404dc7ca76412db7c5f9
baukebrenninkmeijer <bauke.brenninkmeijer@gmail.com>
2017-06-07 21:35:39 +0200

    Get rekt jan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
+export class Player {
+  id: number;
+  name: string;
+  nr_of_pitchers: number;
+}
+
+const PLAYERS: Player[] = [
+  {id: 1, name: 'Bauke', nr_of_pitchers : 0},
+  {id: 2, name: 'Roland', nr_of_pitchers : 0},
+  {id: 3, name: 'Jochem', nr_of_pitchers : 0},
+  {id: 4, name: 'Jan', nr_of_pitchers : 0},
+  {id: 5, name: 'Sebastiaan', nr_of_pitchers : 0}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = "wiehaaltdepitcher";
+  title = 'Wie haalt de pitcher?';
+  players = PLAYERS;
 }
