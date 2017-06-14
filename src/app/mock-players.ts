d6ade8d2be549701cb79609bc52b6249583c91f6
jantjee314 <jan-martens@hotmail.com>
2017-06-14 10:43:42 +0200

    Added basic functionality to be continued

--- src/app/mock-players.ts
+++ src/app/mock-players.ts
@@ -1,9 +1,9 @@
 import {Player} from './player';
 
 export const PLAYERS: Player[] = [
-  {id: 1, name: 'Bauke', nr_of_pitchers : 0},
-  {id: 2, name: 'Roland', nr_of_pitchers : 0},
-  {id: 3, name: 'Jochem', nr_of_pitchers : 0},
-  {id: 4, name: 'Jan', nr_of_pitchers : 0},
-  {id: 5, name: 'Sebastiaan', nr_of_pitchers : 0}
+  {id: 1, name: 'Bauke', nr_of_pitchers : 0, selected: true},
+  {id: 2, name: 'Roland', nr_of_pitchers : 0, selected: true},
+  {id: 3, name: 'Jochem', nr_of_pitchers : 0, selected: true},
+  {id: 4, name: 'Jan', nr_of_pitchers : 0, selected: true},
+  {id: 5, name: 'Sebastiaan', nr_of_pitchers : 0, selected: true}
 ];
