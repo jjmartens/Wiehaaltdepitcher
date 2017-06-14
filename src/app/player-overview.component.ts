b3d061810741ef41329d450f37245b2edce8f592
baukebrenninkmeijer <bauke.brenninkmeijer@gmail.com>
2017-06-14 20:08:49 +0200

    fd

--- src/app/player-overview.component.ts
+++ src/app/player-overview.component.ts
@@ -14,17 +14,37 @@ export class PlayerOverview implements OnInit {
   // players = PLAYERS;
   players: Player[];
   selectedPlayer: Player;
+  pitcherBoy: Player;
+
+  constructor(private playerService: PlayerService){
+  }
+  ngOnChange(): void {
+        this.players.sort(function(a:Player ,b:Player): number { return (a.nr_of_pitchers > b.nr_of_pitchers) ? -1 : ((b.nr_of_pitchers > a.nr_of_pitchers) ? 1 : 0); });
+
+  }
+  ngOnInit(): void {
+    this.getPlayers();
+    this.players.sort(function(a:Player ,b:Player): number { return (a.nr_of_pitchers > b.nr_of_pitchers) ? 1 : ((b.nr_of_pitchers > a.nr_of_pitchers) ? -1 : 0); });
+  }
+
+  roll() {
+    var rand = Math.floor(Math.random() * this.players.length);
+    this.pitcherBoy = this.players[rand];
+  }
+
   onSelect(player: Player): void {
     this.selectedPlayer = player;
   }
   getPlayers(): void {
     this.playerService.getPlayers().then(players => this.players = players);
   }
-
-  constructor(private playerService: PlayerService){
+  cowardFarmer(): void{
+    this.pitcherBoy = null;
   }
 
-  ngOnInit(): void {
-    this.getPlayers();
+  fetchedPitcher(player: Player): void {
+    player.nr_of_pitchers += 1; 
+    this.pitcherBoy = null;
+    this.ngOnChange();  
   }
 }
