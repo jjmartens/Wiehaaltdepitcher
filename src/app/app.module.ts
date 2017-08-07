712b0bae3cb5adb7e58ed27aaff988200005645d
Jan Martens <jan-martens@hotmail.com>
2017-08-07 23:37:13 +0200

    progress with back-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule }    from '@angular/http';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+	HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
